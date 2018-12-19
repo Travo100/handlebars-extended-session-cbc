2107eb8ff53d9c3f4e7ed555561b7761eaa87b3f
Travo100 <travo100@gmail.com>
2018-12-18 16:49:04 -0800

    Sucessfully got the database and server to talk to one another

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
+require('dotenv').config();
 var express = require("express");
 var app = express();
-
+var mysql  = require('mysql');
 var PORT = process.env.PORT || 8080;
+ 
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : process.env.DBPASSWORD,
+    database : 'schoolDB',
+    port: 3306
+});
+
+connection.connect(function(err){
+    if(err) throw err;
+    console.log(connection.threadId);
+});
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.get("/", function(req, res){
-    res.send("Welcome home!");
+    connection.query("SELECT * FROM student", function(err, data){
+        res.json(data);
+    });
 });
 
 app.post("/api/new", function(req, res){
